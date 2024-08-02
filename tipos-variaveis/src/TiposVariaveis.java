86bd377dec91bcf0634f1a3ee0fd8b3dd7538d84
Lucas Brandao <lucasleo270@gmail.com>
2024-08-01 23:05:25 -0300

    Update TiposVariaveis.java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,30 @@
 public class TiposVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        /**Para declarar váriáveis no java precisamos primeiro declarar o tipo de variável depois o nome da variável, seguido de seu valor. Se essa variável for uma constante precisa utilizar a palavra reservada final antes do tipo. Ex: final int ANO_NASCIMENTO = 1990; */
+    
+        final int ANO_NASCIMENTO = 1996;
+        System.out.println(ANO_NASCIMENTO);
+
+        String primeiroNome = "Lucas";
+        String segundoNome = "Leonardo";
+        int idade = 27;
+        double peso = 74.56;
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println("O seu nome completo é " + nomeCompleto + " você tem " + idade + " Anos e pesa " + peso +" kgs " );
+        
     }
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+
+    return primeiroNome.concat(" ").concat(segundoNome); }
+
+
+
+
 }
